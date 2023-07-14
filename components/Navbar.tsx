e7faa07364342dc00ad98ae5771411c1797be9df
Shaleen <shaleengasherry@gmail.com>
2023-07-14 11:17:36 +0300

    Changes

---
+++ components/Navbar.tsx
@@ -0,0 +1,84 @@
+import { useCallback, useEffect, useState } from "react";
+import MobileMenu from "./MobileMenu";
+import NavbarItem from "./NavbarItem";
+import { BellIcon, MagnifyingGlassIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 66;
+
+const Navbar = () => {
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [showBackground, setShowBackground] = useState(false);
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current);
+    }, []);
+
+    const toggleAccountMenu = useCallback(() =>{
+        setShowAccountMenu((current) => !current);
+    }, [])
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            console.log(window.scrollY)
+            if (window.scrollY >= TOP_OFFSET) {
+              setShowBackground(true)
+            } else {
+              setShowBackground(false)
+            }
+          }
+      
+          window.addEventListener('scroll', handleScroll);
+      
+          return () => {
+            window.removeEventListener('scroll', handleScroll);
+          }
+    }, [])
+
+
+    return (
+        <nav className="w-full fixed z-40">
+            <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
+                {/* Logo */}
+                <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo" />
+                {/* Labels on Desktop */}
+                <div className="flex-row ml-8 gap-7 hidden lg:flex">
+                    <NavbarItem label="Home" active />
+                    <NavbarItem label="Series" />
+                    <NavbarItem label="Films" />
+                    <NavbarItem label="New & Popular" />
+                    <NavbarItem label="My List" />
+                    <NavbarItem label="Browse by Languages" />
+                </div>
+                {/* Labels on Mobile */}
+                <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+                    <p className="text-white text-sm">Browse</p>
+                    <ChevronDownIcon className={`w-4 text-white fill-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
+                    <MobileMenu visible={showMobileMenu} />
+                </div>
+                {/* Profiles Section : Search Bar, ***Type of Account, Notifications Bell and Profile */}
+                <div className="flex flex-row ml-auto gap-7 items-center">
+                    <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                        <MagnifyingGlassIcon className="w-6" />
+                    </div>
+                    <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                        <BellIcon className="w-6" />
+                    </div>
+                    <div className="text-white hover:text-gray-300 cursor-pointer transition lg:block hidden"> Kids
+                    </div>
+                    <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
+                        <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+                            <img src="/images/default-red.png" alt=""/>
+                        </div>
+                        <ChevronDownIcon className={`w-4 text-white fill-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+                        <AccountMenu visible={showAccountMenu} />
+                    </div>
+                </div>
+            </div>
+        </nav >
+    )
+}
+
+export default Navbar;
